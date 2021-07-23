2f48adc52c0c43a7167800eef5953ef9e2684f39
Peace Kotamnives <pk568@cornell.edu>
2021-07-23 13:21:07 -0400

    fix bugs: non numeric outputs have been taken cared

--- python/check_tty_status.py
+++ python/check_tty_status.py
@@ -35,30 +35,31 @@ n = 0
 output_file = open('../output/'+timeStamped('check')+file2.split('_')[-1],'w')
 for line in Lines:
     two_files_list = line.split('|')
-    apollo09_list = two_files_list[0].split()
-    file2_list = two_files_list[1].split()
-    ls_err = [] 
-    error_name = ""
-    for name in command_names:
-        n = int(apollo09_list[0])
-        if int(apollo09_list[0]) > command_lines[command_names.index(name)] :
-            error_name = name
-    if len(apollo09_list) - len(file2_list) == 1 :
-    	for i in range(len(apollo09_list)-1):
-            if apollo09_list[i+1] != file2_list[i]:
-                apollo09_value = float(apollo09_list[i+1])
-                file2_value = float(file2_list[i])
-                if abs((file2_value - apollo09_value)*100/apollo09_value) > err :
-                    ls_err.append(str(round(abs((file2_value - apollo09_value)*100/apollo09_value)))+"%")
-    else :
-        n = int(apollo09_list[0])
-        ls_err.append('non-numeric difference')
-    if len(ls_err) > 0 :
-        output_file.write(error_name+"\n")
-        if 'VALUE' in apollo09_list :
-            output_file.write(Lines1[n-2])
-        output_file.write(line.replace(str(n),''))
-        output_file.writelines("failed -> err : %s\n" % item for item in ls_err)
+    if (len(two_files_list)==2):
+         apollo09_list = two_files_list[0].split()
+         file2_list = two_files_list[1].split()
+         ls_err = [] 
+         error_name = ""
+         for name in command_names:
+             n = int(apollo09_list[0])
+             if int(apollo09_list[0]) > command_lines[command_names.index(name)] :
+                 error_name = name
+         if len(apollo09_list) - len(file2_list) == 1 :
+    	     for i in range(len(apollo09_list)-1):
+                 if apollo09_list[i+1] != file2_list[i] and apollo09_list[i+1].isnumeric() and file2_list[i].isnumeric():
+                     apollo09_value = float(apollo09_list[i+1])
+                     file2_value = float(file2_list[i])
+                     if abs((file2_value - apollo09_value)*100/apollo09_value) > err :
+                         ls_err.append(str(round(abs((file2_value - apollo09_value)*100/apollo09_value)))+"%")
+         else :
+             n = int(apollo09_list[0])
+             ls_err.append('non-numeric difference')
+         if len(ls_err) > 0 :
+             output_file.write(error_name+"\n")
+             if 'VALUE' in apollo09_list :
+                 output_file.write(Lines1[n-2])
+             output_file.write(line.replace(str(n),''))
+             output_file.writelines("failed -> err : %s\n" % item for item in ls_err)
 
 diff_file.close()
 output_file.close()
