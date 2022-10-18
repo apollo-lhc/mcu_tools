300b18636704ae338768c6617af98679ceca5902
Peace Kotamnives <pk568@cornell.edu>
2022-10-18 18:09:14 -0400

    modify LoadClk script to avoid dependency to numpy and pandas

--- python/LoadClkConfig_tripletEEPROM.py
+++ python/LoadClkConfig_tripletEEPROM.py
@@ -6,8 +6,6 @@ import io
 import argparse
 import os
 import pdb
-import numpy as np
-import pandas as pd
 
 dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -29,23 +27,23 @@ args = parser.parse_args()
 if args.synth_id == "r0a" :
     # SI5341A
     # occupies 32 EEPROM pages 0x00-0x1f for one specified config file 
-    eeprom_pages = [np.base_repr(i,base=16) for i in np.arange(0,32,1,dtype=np.uint8)] 
+    eeprom_pages = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '1A', '1B', '1C', '1D', '1E', '1F'] 
 elif args.synth_id == "r0b" :
     # SI5395A
     # occupies 32 EEPROM pages 0x20-0x3f for one specified config file
-    eeprom_pages = [np.base_repr(i,base=16) for i in np.arange(32,64,1,dtype=np.uint8)] 
+    eeprom_pages = ['20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '2A', '2B', '2C', '2D', '2E', '2F', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '3A', '3B', '3C', '3D', '3E', '3F'] 
 elif args.synth_id == "r1a" :
     # SI5341
     # occupies 32 EEPROM pages 0x40-0x5f for one specified config file
-    eeprom_pages = [np.base_repr(i,base=16) for i in np.arange(64,96,1,dtype=np.uint8)] 
+    eeprom_pages = ['40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '4A', '4B', '4C', '4D', '4E', '4F', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '5A', '5B', '5C', '5D', '5E', '5F'] 
 elif args.synth_id == "r1b" :
     # SI5341
     # occupies 32 EEPROM pages 0x60-0x7f for one specified config file
-    eeprom_pages = [np.base_repr(i,base=16) for i in np.arange(96,128,1,dtype=np.uint8)] 
+    eeprom_pages = ['60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '6A', '6B', '6C', '6D', '6E', '6F', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '7A', '7B', '7C', '7D', '7E', '7F'] 
 elif args.synth_id == "r1c" :
     # SI5341
     # occupies 32 EEPROM pages 0x80-0x9f for one specified config file
-    eeprom_pages = [np.base_repr(i,base=16) for i in np.arange(128,160,1,dtype=np.uint8)] 
+    eeprom_pages = ['80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '8A', '8B', '8C', '8D', '8E', '8F', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '9A', '9B', '9C', '9D', '9E', '9F'] 
 
 
 serPort = "/dev/"+args.tty
@@ -83,11 +81,11 @@ def write_reg(ListOfRegs,Page_i,Noisy):
          
         if ( 0 < counter < 126): # the triplet version is assigned to have a maximum of 126 data or addresses per EEPROM page
             if (counter < 16):
-                command = "i2cwr "+i2c_port+" "+i2c_addr+" 2" +" 0x"+eeprom_pages[Page]+str(np.base_repr(counter,16,padding=1))+" 1 "+register+"" 
+                command = "i2cwr "+i2c_port+" "+i2c_addr+" 2" +" 0x"+eeprom_pages[Page]+"0"+str(hex(counter).lstrip('0x'))+" 1 "+register+"" 
             else:
-                command = "i2cwr "+i2c_port+" "+i2c_addr+" 2" +" 0x"+eeprom_pages[Page]+str(np.base_repr(counter,16))+" 1 "+register+""         
+                command = "i2cwr "+i2c_port+" "+i2c_addr+" 2" +" 0x"+eeprom_pages[Page]+str(hex(counter).lstrip('0x'))+" 1 "+register+""         
             if Noisy:
-                print(get_command(command))
+                print(get_command(command)[-1])
             else:
                 get_command(command)
             counter += 1
@@ -99,24 +97,25 @@ def write_reg(ListOfRegs,Page_i,Noisy):
             # write the new page number to address 0x01  
             command = "i2cwr "+i2c_port+" "+i2c_addr+" 1 0x01 1 "+eeprom_pages[Page]+""
             if Noisy:
-                print(get_command(command))
+                print(get_command(command)[-1])
             else:
                 get_command(command) 
-            command = "i2cwr "+i2c_port+" "+i2c_addr+" 2" +" 0x"+eeprom_pages[Page]+str(np.base_repr(counter,16,padding=2))+" 1 "+register+""
+            command = "i2cwr "+i2c_port+" "+i2c_addr+" 2" +" 0x"+eeprom_pages[Page]+"00"+" 1 "+register+""
 
             if Noisy:
-                print(get_command(command))
+                print(get_command(command)[-1])
             else:
                 get_command(command) 
             counter += 1
+    print(counter)
 def write_counter(reg_counter,page_counter,nbyte,Page_i,Noisy):
     Page = Page_i 
     if (page_counter < 16):
-        command = "i2cwr "+i2c_port+" "+i2c_addr+" 2" +" 0x"+eeprom_pages[Page]+str(np.base_repr(page_counter,16,padding=1))+" "+str(nbyte)+" "+str(np.base_repr(reg_counter,16))+""
+        command = "i2cwr "+i2c_port+" "+i2c_addr+" 2" +" 0x"+eeprom_pages[Page]+"0"+str(hex(page_counter).lstrip('0x'))+" "+str(nbyte)+" "+str(hex(reg_counter).lstrip('0x'))+""
     else:
-        command = "i2cwr "+i2c_port+" "+i2c_addr+" 2" +" 0x"+eeprom_pages[Page]+str(np.base_repr(page_counter,16))+" "+str(nbyte)+" "+str(np.base_repr(reg_counter,16))+""
+        command = "i2cwr "+i2c_port+" "+i2c_addr+" 2" +" 0x"+eeprom_pages[Page]+str(hex(page_counter).lstrip('0x'))+" "+str(nbyte)+" "+str(hex(reg_counter).lstrip('0x'))+""
     if Noisy:
-        print(get_command(command))
+        print(get_command(command)[-1])
     else:
         get_command(command) 
  
@@ -127,9 +126,8 @@ print(get_command("gpio get ID_EEPROM_WP"))
 print(get_command("gpio set ID_EEPROM_WP 0"))
 print(get_command("gpio get ID_EEPROM_WP"))
 # open csv files and create/initialize variables
-df = pd.read_csv(args.Reg_List, header=None, delimiter=',', names=['Address','Data'])
-Address_List = df['Address'].tolist()
-Data_List = df['Data'].tolist()
+regfile = open(args.Reg_List, 'r')
+
 PreambleList = []
 PostambleList = []
 RegisterList = []
@@ -139,44 +137,49 @@ InPostamble = 0
 
 
 # read the desired register settings from the "register" file
-    
-for i in range(len(Address_List)): 
-    reg = Address_List[i]
-    val = Data_List[i] 
-    # append the register address and value to the appropriate list
-    if (reg.find("Start") != -1 and reg.find("preamble") != -1):
-        InPreamble = 1
-        continue
-    if (reg.find("End") != -1 and reg.find("preamble") != -1):
-        InPreamble = 0
-        continue
-    if (reg.find("Start") != -1 and reg.find("register") != -1):
-        InRegisters = 1
-        continue
-    if (reg.find("End") != -1 and reg.find("register") != -1):
-        InRegisters = 0
-        continue
-    if (reg.find("Start") != -1 and reg.find("postamble") != -1):
-        InPostamble = 1
-        continue
-    if (reg.find("End") != -1 and reg.find("postamble") != -1):
-        InPostamble = 0
-        break
-    if InPreamble:
-        if ("0x" in reg and val):
-            PreambleList.append(reg[2:4])
-            PreambleList.append(reg[4:6])
-            PreambleList.append(val)
-    elif InRegisters:
-        if ("0x" in reg and val):
-            RegisterList.append(reg[2:4]) 
-            RegisterList.append(reg[4:6])
-            RegisterList.append(val)  
-    elif InPostamble:
-        if ("0x" in reg and val):
-            PostambleList.append(reg[2:4])
-            PostambleList.append(reg[4:6])
-            PostambleList.append(val)
+for line in regfile:
+    for words in line.split('"'):
+        reg_loc = words.find("0x")
+        if (reg_loc == 0):
+            reg = words[reg_loc:reg_loc+6]
+        else:
+            reg = words[:]
+        val_loc = words.find("0x",reg_loc + 1)
+        val = words[val_loc:val_loc+4]
+        # append the register address and value to the appropriate list
+        if (reg.find("Start") != -1 and reg.find("preamble") != -1):
+            InPreamble = 1
+            continue
+        if (reg.find("End") != -1 and reg.find("preamble") != -1):
+            InPreamble = 0
+            continue
+        if (reg.find("Start") != -1 and reg.find("register") != -1):
+            InRegisters = 1
+            continue
+        if (reg.find("End") != -1 and reg.find("register") != -1):
+            InRegisters = 0
+            continue
+        if (reg.find("Start") != -1 and reg.find("postamble") != -1):
+            InPostamble = 1
+            continue
+        if (reg.find("End") != -1 and reg.find("postamble") != -1):
+            InPostamble = 0
+            break
+        if InPreamble:
+            if ("0x" in reg and val):
+                PreambleList.append(reg[2:4])
+                PreambleList.append(reg[4:6])
+                PreambleList.append(val)
+        elif InRegisters:
+            if ("0x" in reg and val):
+                RegisterList.append(reg[2:4]) 
+                RegisterList.append(reg[4:6])
+                RegisterList.append(val)  
+        elif InPostamble:
+            if ("0x" in reg and val):
+                PostambleList.append(reg[2:4])
+                PostambleList.append(reg[4:6])
+                PostambleList.append(val)
 
                  
 # send the preamble, data, and postamble to the eeprom
